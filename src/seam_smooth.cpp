0a0db9d0623a6c693c6822b1b3ef36d4c8d678a6
syyan2 <syyan2@iflytek.com>
2023-12-01 14:08:20 +0800

    update: use eu-distance

--- src/seam_smooth.cpp
+++ src/seam_smooth.cpp
@@ -9,7 +9,7 @@ int SeamSmooth::Run(const cv::Mat& img)
     printf("Input wh = %d,%d\n", img.cols, img.rows);
 
     cv::Mat img_smoothed = seam_smooth(img);
-    cv::imwrite("../data/pano_ros_aseam_20230922_161441/seam_1.png", img_smoothed);
+    cv::imwrite("../data/pano_ros_aseam_20230922_161441/seam_2.png", img_smoothed);
     // cv::namedWindow("img", cv::WINDOW_NORMAL);
     // cv::resizeWindow("img", img_smoothed.size() / 2);
     // cv::imshow("img", img_smoothed);
@@ -23,38 +23,49 @@ cv::Mat SeamSmooth::seam_smooth(const cv::Mat& img)
 {
     cv::Mat img_smooth = img.clone();
 
-    // 每个点的颜色 = 周围一些点的平均值, 如果8邻域某个点颜色差异较大，则忽略该邻域点
-    auto smooth_color = [&] (int i, int j, int channel) {
-        int color = 0;
-        int count = 0;
-        // 3*3 区域
-        const int avg_width = 20;
+    auto diff_color_vec4b = [&] (const cv::Vec4b& color1, const cv::Vec4b& color2) {
+        return std::pow(color1[0] - color2[0], 2) +     // b
+                std::pow(color1[1] - color2[1], 2) +    // g
+                std::pow(color1[2] - color2[2], 2);     // r
+    };
 
-        uchar color_ij = img.at<cv::Vec4b>(i, j)[channel];
-        for (int m=i-avg_width; m<=i+avg_width; ++m) {
+    // 每个点的颜色 = 周围一些点的平均值, 如果8邻域某个点颜色差异较大，则忽略该邻域点
+    auto smooth_color = [&] (int i, int j) {
+        cv::Vec4b color_current = img.at<cv::Vec4b>(i, j);
+        int color_sum_b = color_current[0];
+        int color_sum_g = color_current[1];
+        int color_sum_r = color_current[2];
+        int count = 1;
+
+        // 在一个小区域内求均值(比如5*5)
+        const int avg_height = 5;
+        const int avg_width = 10;
+        for (int m=i-avg_height; m<=i+avg_height; ++m) {
             for (int n=j-avg_width; n<=j+avg_width; ++n) {
                 if (m < 0 || m >= img.rows || n < 0 || n >= img.cols) {
                     continue;
                 }
 
-                uchar color_mn = img.at<cv::Vec4b>(m, n)[channel];
-                if (std::abs(color_mn - color_ij) > 15) {
+                // 颜色差异过大则不用于求均值
+                cv::Vec4b color_around = img.at<cv::Vec4b>(m, n);
+                if (diff_color_vec4b(color_current, color_around) > 440) {
                     continue;
                 }
 
-                color += img.at<cv::Vec4b>(m, n)[channel];
+                color_sum_b += color_around[0];
+                color_sum_g += color_around[1];
+                color_sum_r += color_around[2];
                 count += 1;
             }
         }
 
-        return color / count;
+        cv::Vec4b color_result(color_sum_b/count, color_sum_g/count, color_sum_r/count, color_current[3]);
+        return color_result;
     };
 
     for (int i=0; i<img.rows; ++i) {
         for (int j=0; j<img.cols; ++j) {
-            img_smooth.at<cv::Vec4b>(i, j)[0] = smooth_color(i, j, 0);
-            img_smooth.at<cv::Vec4b>(i, j)[1] = smooth_color(i, j, 1);
-            img_smooth.at<cv::Vec4b>(i, j)[2] = smooth_color(i, j, 2);
+            img_smooth.at<cv::Vec4b>(i, j) = smooth_color(i, j);
         }
     }
 
